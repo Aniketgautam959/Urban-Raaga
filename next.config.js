cb3ac9e58ce541ad3ec6ae2ec7f9d59798b8b5f7
Aniketgautam959 <164925581+Aniketgautam959@users.noreply.github.com>
2026-03-26 10:56:33 +0530

    fix: Add 301 redirect from /home to / to fix 404 error

--- next.config.js
+++ next.config.js
@@ -12,6 +12,15 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/home",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
